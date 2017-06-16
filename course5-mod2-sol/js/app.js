b0531c32a3084c450dad61912c159d1cdd833e95
John L. Cao <john_caohaha@hotmail.com>
2017-06-15 17:15:23 -0700

    course 5 module 2 solution

---
+++ course5-mod2-sol/js/app.js
@@ -0,0 +1,52 @@
+//app.js
+(function(){
+
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService)
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService){
+	var buy = this;
+	buy.items = ShoppingListCheckOffService.getItemsToBuy();
+	buy.buyItem = ShoppingListCheckOffService.buyItem;
+};
+
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService){
+	var bought = this;
+	bought.items = ShoppingListCheckOffService.getItemsAlreadyBought();
+};
+
+// service
+function ShoppingListCheckOffService(){
+	var service = this;
+	var toBuyItems = [
+		{name: 'mint mentos', quantity: 1},
+		{name: 'coke mentos', quantity: 2},
+		{name: 'fruit mentos', quantity: 3},
+		{name: 'brocolli mentos', quantity: 4},
+		{name: 'steak mentos', quantity: 5}
+	];
+	var boughtItems = [];
+
+	service.getItemsToBuy = function(){
+		return toBuyItems;
+	};
+
+	service.getItemsAlreadyBought = function(){
+		return boughtItems;
+	};
+
+	service.buyItem = function(index){
+		boughtItems.push(toBuyItems.splice(index, 1)[0]);
+		console.log(boughtItems);
+	};
+};
+
+
+})(); //IIFE
